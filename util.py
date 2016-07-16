3c9f1f0e7f2f5e82f1bc6e4ed744bc541bb3d6d9
Stephan Hügel <urschrei@gmail.com>
2016-07-16 19:08:07 +0100

    Tidying up

--- util.py
+++ util.py
@@ -3,7 +3,6 @@ from ctypes import Structure, POINTER, c_void_p, c_size_t, c_double, c_char_p, c
 
 from array import array
 import numpy as np
-import ipdb
 
 lib = ctypes.cdll.LoadLibrary("libpolyline_ffi.dylib")
 
@@ -11,7 +10,6 @@ class _FFIArray(Structure):
     """
     Convert sequence of float lists to a C-compatible void array
     example: [[1.0, 2.0], [3.0, 4.0]]
-    Sequence members are _CoordArray instances
 
     """
     _fields_ = [("data", c_void_p),
@@ -19,7 +17,7 @@ class _FFIArray(Structure):
 
     @classmethod
     def from_param(cls, seq):
-        """  Allow implicit conversions from a sequence of 64-bit floats."""
+        """  Allow implicit conversions """
         return seq if isinstance(seq, cls) else cls(seq)
 
     def __init__(self, seq, data_type = c_double):
@@ -31,35 +29,40 @@ class _FFIArray(Structure):
         self.len = len(seq)
 
 
-class _Result(Structure):
-    """ Container for returned FFI data """
+class _CoordResult(Structure):
+    """ Container for returned FFI coordinate data """
     _fields_ = [("coords", _FFIArray)]
 
 
+class _PolylineResult(Structure):
+    """ Container for returned FFI Polyline data """
+    _fields_ = [("line", c_void_p)]
+
+
 def _void_array_to_nested_list(res, _func, _args):
-    """ Convert the FFI result to Python data structures """
+    """ Dereference the FFI result to a list of coordinates """
     coords = [list(pair) for pair in
         ((POINTER(c_double * 2).from_buffer_copy(res.coords)[:res.coords.len]))
         ]
     drop_array(res.coords)
     return coords
 
-def char_array_to_string(res, _func, _args):
-    converted = cast(res, c_char_p)
-    result = converted.value
-    drop_cstring(converted)
-    return result
+def void_array_to_string(res, _func, _args):
+    """ Dereference the FFI result to a utf8 polyline """
+    result = cast(res.line, c_char_p)
+    polyline = result.value
+    drop_cstring(result)
+    return polyline
 
 decode_polyline = lib.decode_polyline_ffi
 decode_polyline.argtypes = (c_char_p,)
-decode_polyline.restype = _Result
+decode_polyline.restype = _CoordResult
 decode_polyline.errcheck = _void_array_to_nested_list
 
 encode_coordinates = lib.encode_coordinates_ffi
 encode_coordinates.argtypes = (_FFIArray,)
-encode_coordinates.restype = c_void_p
-encode_coordinates.errcheck = char_array_to_string
-
+encode_coordinates.restype = _PolylineResult
+encode_coordinates.errcheck = void_array_to_string
 
 # Free FFI-allocated memory
 drop_cstring = lib.drop_cstring
@@ -70,7 +73,8 @@ drop_array = lib.drop_float_array
 drop_array.argtypes = (_FFIArray,)
 drop_array.restype = None
 
-# pl = "_ibE_seK_seK_seK"
-# print decode_polyline(pl)
-print encode_coordinates([[1.0, 2.0], [3.0, 4.0]])
+pl = "_ibE_seK_seK_seK"
+res = decode_polyline(pl)
+print res
+print encode_coordinates(res)
 
