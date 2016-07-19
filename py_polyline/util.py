3503e069814f919eb7bf66519f27f38e934ae1a4
Stephan Hügel <stephan.hugel.12@ucl.ac.uk>
2016-07-19 13:53:46 +0100

    Add version, author, license

--- py_polyline/util.py
+++ py_polyline/util.py
@@ -1,7 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+util.py
+
+Created by Stephan Hügel on 2016-07-19
+
+This file is part of convertbng.
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Stephan Hügel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+"""
+
 import os
 from sys import platform, version_info
 from ctypes import Structure, POINTER, c_void_p, c_size_t, c_double, c_uint32, c_char_p, cast, cdll, string_at
 import numpy as np
+
+__author__ = u"Stephan Hügel"
+__version__ = "0.1.0"
 
 file_path = os.path.dirname(__file__)
 
