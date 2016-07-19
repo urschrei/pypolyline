e9023fcf6c73aa8e8e98df8aad754a48be22c693
Stephan Hügel <stephan.hugel.12@ucl.ac.uk>
2016-07-19 15:34:08 +0100

    Add unit tests

---
+++ py_polyline/test/test.py
@@ -0,0 +1,28 @@
+import unittest
+from py_polyline.util import encode_coordinates, decode_polyline
+
+class PolylineTests(unittest.TestCase):
+    """ Tests for py_polyline """
+
+    def setUp(self):
+        """ make these available to all tests """
+        self.coords = [
+            [52.64125, 23.70162],
+            [52.64938, 23.70154],
+            [52.64957, 23.68546],
+            [52.64122, 23.68549],
+            [52.64125, 23.70162]
+            ]
+        self.polyline = 'ynh`IcftoCyq@Ne@ncBds@EEycB'
+
+    def testDecodePolyline(self):
+        """ Test that Polylines can be decoded """
+        expected = self.coords
+        result = decode_polyline(self.polyline, 5)
+        self.assertEqual(result, expected)
+
+    def testEncodeCoordinates(self):
+        """ Test that coordinates can be encoded """
+        expected = self.polyline 
+        result = encode_coordinates(self.coords, 5)
+        self.assertEqual(result, expected)
