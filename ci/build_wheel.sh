565d50e54cf755f635d39de15a0b5b5b41eb0f53
Stephan Hügel <stephan.hugel.12@ucl.ac.uk>
2016-07-25 16:32:57 +0100

    First pass at CI setup

---
+++ ci/build_wheel.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e -x
+
+PYBINS=(
+  # "/opt/python/cp27-cp27m/bin"
+  "/opt/python/cp27-cp27mu/bin"
+  # "/opt/python/cp33-cp33m/bin"
+  # "/opt/python/cp34-cp34m/bin"
+  "/opt/python/cp35-cp35m/bin"
+  )
+
+mkdir -p /io/wheelhouse
+# ls -la /io
+# ls -la /io/convertbng
+echo $LD_LIBRARY_PATH
+mkdir -p /usr/local/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+export DOCKER_BUILD=true
+cp /io/convertbng/libpolyline_ffi.so /usr/local/lib
+# Compile wheels
+for PYBIN in ${PYBINS[@]}; do
+    ${PYBIN}/pip install -r /io/dev-requirements.txt
+    ${PYBIN}/pip wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair $whl -w /io/wheelhouse/
+done
+
+# Install packages and test
+for PYBIN in ${PYBINS[@]}; do
+    ${PYBIN}/pip install pypolyline --no-index -f /io/wheelhouse
+    (cd $HOME; ${PYBIN}/nosetests pypolyline)
+done
