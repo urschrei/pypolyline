c768be0eb3220470637b16bd82e64a4a240ee939
Stephan Hügel <urschrei@gmail.com>
2018-05-30 11:06:20 +0100

    Switch back to simple header

--- pypolyline/header.h
+++ pypolyline/header.h
@@ -1,34 +1,18 @@
+#ifndef HEADER_H
+#define HEADER_H
 
-#ifndef cheddar_generated_header_h
-#define cheddar_generated_header_h
-
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include <stdint.h>
-#include <stdbool.h>
-
-
-
-/// A C-compatible `struct` used for passing arrays across the FFI boundary
 typedef struct Array {
-    void const* data;
+    void* data;
     size_t len;
 } Array;
 
-Array decode_polyline_ffi(char const* pl, int precision);
+Array decode_polyline_ffi(char* pl, int precision);
 
 char* encode_coordinates_ffi(Array coords, int precision);
 
 void drop_float_array(Array arr);
 
 void drop_cstring(char* p);
-
-#ifdef __cplusplus
-}
-#endif
 
 
 #endif
